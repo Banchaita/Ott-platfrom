46a384b5e45615a91d51f2ee36ed60bbce1931b9
Banchaita <biswassanchaita1998@gmail.com>
2023-06-18 12:10:01 +0530

    Some Changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,11 +7,9 @@ import { signIn } from "next-auth/react";
 const Login = () => {
   return (
     <div className="login_bg_gradient bg-cover h-screen grid place-items-center">
-      <Logo style="w-52 absolute top-0 left-0 m-8" />
-
+      <p className="w-52 absolute top-0 left-0 m-2 text-4xl">WE</p>
       <div className="bg-[rgba(0,0,0,0.75)] p-10 w-80 space-y-6">
         <h2 className="text-3xl font-medium">Sign in</h2>
-
         <button
           className="bg-white text-black flex gap-2 items-center p-4 text-xl"
           onClick={() => signIn("google")}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 import { BiSearch } from "react-icons/bi";
 import { BsBellFill } from "react-icons/bs";
+import {BiLogInCircle} from "react-icons/bi"
 import { signOut } from "next-auth/react";
 
 const Navbar = () => {
@@ -13,7 +14,7 @@ const Navbar = () => {
       <div className="container flex justify-between">
         <div className="flex items-center space-x-2 md:space-x-10">
           <Link href="/">
-            <Logo style="h-auto w-[100px]" />
+            <p className="h-auto w-[100px] text-4xl">WE</p>
           </Link>
 
           <ul className="hidden space-x-4 md:flex">
@@ -35,15 +36,7 @@ const Navbar = () => {
           <BiSearch className="sm hidden h-6 w-6 sm:inline" />
           <p className="hidden lg:inline">Kids</p>
           <BsBellFill className="h-6 w-6" />
-
-          <Image
-            src="/dp.png"
-            alt="dp"
-            width={60}
-            height={60}
-            className="cursor-pointer rounded w-auto h-auto"
-            onClick={() => signOut()}
-          />
+          <BiLogInCircle className="h-6 w-6 cursor-pointer rounded" onClick={() => signOut()}/> 
         </div>
       </div>
     </nav>
