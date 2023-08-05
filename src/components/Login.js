5278043669657a69ee5095162acb1b396738fd5a
Banchaita <biswassanchaita1998@gmail.com>
2023-08-05 15:05:15 +0530

    Edited Code

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,7 @@ const Login = () => {
   return (
     <div className="login_bg_gradient bg-cover h-screen grid place-items-center">
       <p className="w-52 absolute top-0 left-0 m-2 text-4xl">WE</p>
+      {/* <p className="w-52 absolute top-0 left-0 m-2 text-4xl">getUser</p> */}
       <div className="bg-[rgba(0,0,0,0.75)] p-10 w-80 space-y-6">
         <h2 className="text-3xl font-medium">Sign in</h2>
         <button
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,93 @@
 import Link from "next/link";
 import React from "react";
-import Logo from "./Logo";
-import Image from "next/image";
+import { useRouter } from "next/router";
+import { signOut } from "next-auth/react";
 
 import { BiSearch } from "react-icons/bi";
 import { BsBellFill } from "react-icons/bs";
-import {BiLogInCircle} from "react-icons/bi"
-import { signOut } from "next-auth/react";
+import { BiLogInCircle } from "react-icons/bi";
 
 const Navbar = () => {
+  const router = useRouter();
+  const isIndexPage = router.pathname === "/"; // Check if the current page is the index page
+  const isStoryTellerPage = router.pathname === "/StoryTeller"; // Check if the current page is the index page
+  const isMusiconrPage = router.pathname === "/musicon"; // Check if the current page is the index page
+
   return (
     <nav>
       <div className="container flex justify-between">
         <div className="flex items-center space-x-2 md:space-x-10">
+          {/* Logo and Home link */}
           <Link href="/">
             <p className="h-auto w-[100px] text-4xl">WE</p>
           </Link>
+          {!isStoryTellerPage || isIndexPage  &&  ( // Hide this UL on the index page
+            <ul className="hidden space-x-4 md:flex">
+              <Link href="/">
+                <li className="headerLink cursor-pointer font-semibold text-white hover:text-white">
+                  Home
+                </li>
+              </Link>
+              <Link href="/StoryTeller">
+                <li className="headerLink">Story Teller</li>
+              </Link>
+              <Link href="/musicon">
+                <li className="headerLink">Musicon</li>
+              </Link>
+              <Link href="/LifeStyle">
+                <li className="headerLink">Life Style</li>
+              </Link>
+              <Link href="/SoundTrack">
+                <li className="headerLink">Sound Track</li>
+              </Link>
+            </ul>
+          )}
+          <ul className="hidden space-x-4 md:flex">
+              {isStoryTellerPage && (
+                <>
+                  <Link href="/StoryTeller">
+                    <li className="headerLink cursor-pointer font-semibold text-white hover:text-black">Home</li>
+                  </Link>
+                  <li className="headerLink">Best Movies</li>
+                  <li className="headerLink">New & Popular</li>
+                  <li className="headerLink">TV Shows</li>
+                  <li className="headerLink">My List</li>
+                </> 
+              )}
+          </ul>
 
           <ul className="hidden space-x-4 md:flex">
-            <li className="headerLink cursor-pointer font-semibold text-white hover:text-white">
-              Home
-            </li>
-
-            <li className="headerLink">TV Shows</li>
-            <li className="headerLink">
-              
-              Movies
-              </li>
-            <li className="headerLink">New & Popular</li>
-            <li className="headerLink">My List</li>
+              {isMusiconrPage && (
+                <>
+                  <Link href="/StoryTeller">
+                    <li className="headerLink cursor-pointer font-semibold text-white hover:text-black">Home</li>
+                  </Link>
+                  <li className="headerLink">Top Musicon </li>
+                  <li className="headerLink">New & Popular</li>
+                  <li className="headerLink">Best Musicon</li>
+                  <li className="headerLink">My List</li>
+                </> 
+              )}
           </ul>
+          
         </div>
 
         <div className="flex items-center space-x-4 text-sm font-light">
           <BiSearch className="sm hidden h-6 w-6 sm:inline" />
-          <p className="hidden lg:inline">Kids</p>
+          {!isIndexPage && <p className="hidden lg:inline">Kids</p>} {/* Show this only on other pages */}
           <BsBellFill className="h-6 w-6" />
-          <BiLogInCircle className="h-6 w-6 cursor-pointer rounded" onClick={() => signOut()}/> 
+          {isIndexPage && ( // Show this only on the index page
+            <BiLogInCircle
+              className="h-6 w-6 cursor-pointer rounded"
+              onClick={() => signOut()}
+            />
+          )}
+          {!isIndexPage && (
+            <BiLogInCircle
+              className="h-6 w-6 cursor-pointer rounded"
+              onClick={() => signOut()}
+            />
+          )}
         </div>
       </div>
     </nav>
